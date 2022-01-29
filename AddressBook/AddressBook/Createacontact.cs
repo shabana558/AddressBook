82c0e03b004cbcc2d727d1c3ca7f096f334bd1f7
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-29 15:53:56 +0530

    [shabana] added UC2-Newcontact

--- AddressBook/AddressBook/Createacontact.cs
+++ AddressBook/AddressBook/Createacontact.cs
@@ -8,64 +8,85 @@ namespace AddressBook
 {
     internal class Createacontact
     {
-        public static void  main()
-
-        {    
-           Console.WriteLine("Welcome to my address book program");   
-            Console.WriteLine("Press 1 to Add an entry to the address book");
-            Console.WriteLine("Press 2 to Remove any entry in the address book");
-            Console.WriteLine("Press 3 to Print the book to the screen");
-            Console.WriteLine("Press 4 to Edit a record");
-            Console.WriteLine("Press 5 to Search for a record");
-            Console.ReadLine();
 
 
-            Console.WriteLine("Please Enter your Lastname");
-            string str2 = "Lastname";
-            str2 = Console.ReadLine();
-            string valueString2 = str2;
+        public static List<Person> People = new List<Person>();
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Addresses { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string ZipCode { get; set; }
+            public string PhoneNum { get; set; }
+            public string EmailId { get; set; }
+        }
 
-            Console.WriteLine("Please Enter your Firstname");
-            string str1 = "Firstname";
-            str1 = Console.ReadLine();
-            string valueString1 = str1;
+        internal static void PrintCustomer(object person)
+        {
+            throw new NotImplementedException();
+        }
 
-            Console.WriteLine("Please Enter your Address, phonenumber and email");
-            string str3 = "Address";
-            str3 = Console.ReadLine();
-            string valueString3 = str3;
+        public static void GetCustomer()
+        {
+            Person person = new Person();
 
-            Console.WriteLine("Please Enter your phonenumber");
-            string str5 = "phonenumber";
-            str5 = Console.ReadLine();
-            string valueString5 = str5;
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
 
-            Console.WriteLine("Please Enter your Postcode");
-            string str6 = "Postcode";
-            str6 = Console.ReadLine();
-            string valueString6 = str6;
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
 
-            Console.WriteLine("Please Enter your email");
-            string str7 = "email";
-            str6 = Console.ReadLine();
-            string valueString7 = str7;
+            Console.Write("Enter Address : ");
+            person.Addresses = Console.ReadLine();
 
+            Console.Write("Enter City : ");
+            person.City = Console.ReadLine();
 
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
 
-            Console.WriteLine("Here is a list of the stored names and addresees that you have entered so far");
-            Console.ReadLine();
+            Console.Write("Enter ZipCode: ");
+            person.ZipCode = Console.ReadLine();
 
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNum = Console.ReadLine();
 
-           
+            Console.Write("Enter EmailId: ");
+            person.EmailId = Console.ReadLine();
 
-            
-            Console.WriteLine("Lastname you entered: " + valueString2);
-            Console.WriteLine("Firstname you entered: " + valueString1);
-            Console.WriteLine("Address and house number and town you entered: " + valueString3);
-            Console.WriteLine("County you entered: " + valueString5);
-            Console.WriteLine("Postcode you entered: " + valueString6);
-            Console.ReadLine();
-
+            People.Add(person);
+        }
+        public static void PrintCustomer(Person person)
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Address : " + person.Addresses);
+            Console.WriteLine("City : " + person.City);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.ZipCode);
+            Console.WriteLine("Phone Number: " + person.PhoneNum);
+            Console.WriteLine("Email Id: " + person.EmailId);
+            Console.WriteLine("-------------------------------------------");
+        }
+        public static void ListingPeople()
+        {
+            if (People.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var person in People)
+            {
+                PrintCustomer(person);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
         }
        
 
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -9,69 +9,20 @@ namespace AddressBook
     internal class Program
     {
         static void Main(string[] args)
-        {
-            {
+        { 
 
-                Console.WriteLine("Welcome to my address book program");
-                Console.WriteLine("Press 1 to Add an entry to the address book");
-                Console.WriteLine("Press 2 to Remove any entry in the address book");
-                Console.WriteLine("Press 3 to Print the book to the screen");
-                Console.WriteLine("Press 4 to Edit a record");
-                Console.WriteLine("Press 5 to Search for a record");
-                Console.ReadLine();
+        Console.WriteLine("Welcome To Address Book System!");
+            Createacontact.GetCustomer();
+            Createacontact.ListingPeople();
 
 
-                Console.WriteLine("Please Enter your Lastname");
-                string str2 = "Lastname";
-                str2 = Console.ReadLine();
-                string valueString2 = str2;
-
-                Console.WriteLine("Please Enter your Firstname");
-                string str1 = "Firstname";
-                str1 = Console.ReadLine();
-                string valueString1 = str1;
-
-                Console.WriteLine("Please Enter your Address, phonenumber and email");
-                string str3 = "Address";
-                str3 = Console.ReadLine();
-                string valueString3 = str3;
-
-                Console.WriteLine("Please Enter your phonenumber");
-                string str5 = "phonenumber";
-                str5 = Console.ReadLine();
-                string valueString5 = str5;
-
-                Console.WriteLine("Please Enter your Postcode");
-                string str6 = "Postcode";
-                str6 = Console.ReadLine();
-                string valueString6 = str6;
-
-                Console.WriteLine("Please Enter your email");
-                string str7 = "email";
-                str6 = Console.ReadLine();
-                string valueString7 = str7;
-
-
-
-                Console.WriteLine("Here is a list of the stored names and addresees that you have entered so far");
-                Console.ReadLine();
-
-
-
-
-
-                Console.WriteLine("Lastname you entered: " + valueString2);
-                Console.WriteLine("Firstname you entered: " + valueString1);
-                Console.WriteLine("Address and house number and town you entered: " + valueString3);
-                Console.WriteLine("County you entered: " + valueString5);
-                Console.WriteLine("Postcode you entered: " + valueString6);
-                Console.ReadLine();
-
-            }
-           
 
 
         }
+
+
+
+
     }
-    
 }
+
