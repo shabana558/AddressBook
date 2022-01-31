60d4a9cc2b0464646bd6b47c2380c2148a3ce968
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-31 16:43:25 +0530

    [shabana] added UC4-Modifycontact

--- AddressBook/AddressBook/Createacontact.cs
+++ AddressBook/AddressBook/Createacontact.cs
@@ -142,6 +142,24 @@ namespace AddressBook
 
             }
         }
+        //Removing the detail
+        public static void RemovePeople()
+        {
+            Console.WriteLine("Enter the first name of the person you would like to remove.");
+            string Remove = Console.ReadLine();
+            foreach (var person in People.ToList())
+            {
+                if (person.FirstName.ToUpper() == Remove.ToUpper())
+                {
+                    People.Remove(person);
+                    Console.WriteLine("Contact is deleted");
+                }
+                else
+                {
+                    Console.WriteLine("Contact is not present");
+                }
+            }
+        }
         public static void ListingPeople()
         {
             if (People.Count == 0)
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -8,23 +8,52 @@ namespace AddressBook
 {
     internal class Program
     {
-        static void Main(string[] args)
-        { 
+        public static object Sample { get; private set; }
 
-        Console.WriteLine("Welcome To Address Book System!");
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome To Address Book System!");
             Createacontact.GetCustomer();
             AddressBook.Createacontact.Modify();
             Createacontact.ListingPeople();
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Sample.Createacontact.GetCustomer();
+                    Sample.Createacontact.ListingPeople();
+                    break;
+                case "2":
+                    Sample.Createacontact.GetCustomer();
+                    Sample.Createacontact.Modify();
+                    Sample.Createacontact.ListingPeople();
+                    break;
+                case "3":
+                    Sample.Createacontact.GetCustomer();
+                    Sample.Createacontact.ListingPeople();
+                    break;
+                case "4":
+                    Sample.Createacontact.GetCustomer();
+                    Sample.Createacontact.RemovePeople();
 
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+
+
+
+
+
+            }
 
 
 
 
         }
-
-
-
-
     }
 }
 
